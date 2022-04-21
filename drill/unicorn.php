8575876e5148457a9fd4e85480274a4feaa89e7f
Anaïs Bossouw <anais.bossouw@gmail.com>
2022-04-21 11:58:16 +0200

    Fini jusque unicorn

--- drill/unicorn.php
+++ drill/unicorn.php
@@ -20,9 +20,9 @@
 
     <?php
 
-        $Human = <iframe src="https://giphy.com/embed/w3AOSBckIzlrod6YCa" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/helloall-pride-queer-human-w3AOSBckIzlrod6YCa">via GIPHY</a></p>;
-        $Cat = <iframe src="https://giphy.com/embed/onOWJOc7U5GAE" width="480" height="253" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cat-sup-whats-up-onOWJOc7U5GAE">via GIPHY</a></p>;
-        $Unicorn = <iframe src="https://giphy.com/embed/lNAjbd8WRhWFy" width="376" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/my-little-pony-lNAjbd8WRhWFy">via GIPHY</a></p>;
+        $Human = "<iframe src='https://giphy.com/embed/w3AOSBckIzlrod6YCa' width='480' height='480' frameBorder='0' class='giphy-embed' allowFullScreen></iframe><p><a href='https://giphy.com/gifs/helloall-pride-queer-human-w3AOSBckIzlrod6YCa'>via GIPHY</a></p>";
+        $Cat = "<iframe src='https://giphy.com/embed/onOWJOc7U5GAE' width='480' height='253' frameBorder='0' class='giphy-embed' allowFullScreen></iframe><p><a href='https://giphy.com/gifs/cat-sup-whats-up-onOWJOc7U5GAE'>via GIPHY</a></p>";
+        $Unicorn = "<iframe src='https://giphy.com/embed/lNAjbd8WRhWFy' width='376' height='480' frameBorder='0' class='giphy-embed' allowFullScreen></iframe><p><a href='https://giphy.com/gifs/my-little-pony-lNAjbd8WRhWFy'>via GIPHY</a></p>";
     
         echo($_GET['species'] == 'Human'? $Human : ($_GET['species'] == 'Cat' ? $Cat : $Unicorn));
     ?>
